e00265de2ccb87c2cfdbf2f72e36ac5ab79fd98e
Viswajith2003 <viswajithviswa715@gmail.com>
2025-06-11 11:46:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App bg-gray-200 p-4 flex justify-center">
+      <h1 className="text-3xl font-bold text-blue-600 ">
+        Hello Tailwind CSS!
+      </h1>
+      <p className="text-lg mx-4">
+        This is a sample app using Tailwind CSS.
+      </p>
+    </div>
+  )
+}
+
+export default App
